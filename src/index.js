dfb7901abc14e1e42c6a40caf153ad2aed073f28
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-05-30 23:25:24 +0900

    Add to convert markdown 2 html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 `use strict`
 
 const { promises : fs } = require('fs');
+const marked = require('marked');
 
 fs.readFile( process.argv[2], { encoding: 'utf-8' }).then( text => {
-  console.log(text);
+
+  console.log(marked(text));
 });
