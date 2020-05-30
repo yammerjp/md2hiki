708ec1c901b7ab88c64b7533d66720c4d403af4d
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-05-30 23:45:12 +0900

    Add born

--- src/index.js
+++ src/index.js
@@ -2,8 +2,66 @@
 
 const { promises : fs } = require('fs');
 const marked = require('marked');
+const { html2json } = require('html2json');
 
-fs.readFile( process.argv[2], { encoding: 'utf-8' }).then( text => {
+let printString = '';
+function print(text) {
+  printString += text;
+}
+function printStdout() {
+  console.log(printString);
+};
 
-  console.log(marked(text));
+function printNode(node) {
+  if( node.node === 'text' ) {
+    print(node.text);
+    return;
+  }
+  if( node.node !== 'element' ) {
+    console.error(`unknown node : ${node}`);
+    process.exit(1);
+  }
+  switch ( node.tag ) {
+    case 'h1':
+      print('!');
+      node.child.forEach( node => printNode(node) );
+      return;
+    case 'h2':
+      print('!!');
+      node.child.forEach( node => printNode(node) );
+      return;
+    default :
+      console.error(node);
+  }
+}
+
+fs.readFile( process.argv[2], { encoding: 'utf-8' }).then( markdown => {
+
+  const html = marked( markdown );
+  const dom = html2json( html );
+
+  if ( dom.node !== 'root') {
+    console.error('parse html2json is failed');
+    process.exit(1);
+  } 
+
+  dom.child.forEach( node => {
+    printNode(node);
+  });
+
+  printStdout();
 });
+
+
+/*
+{
+  node: 'root',
+  child: [
+    { node: 'element', tag: 'h1', attr: [Object], child: [Array] },
+    { node: 'text', text: '\n' },
+    { node: 'element', tag: 'h2', attr: [Object], child: [Array] },
+    { node: 'text', text: '\n' }
+  ]
+}
+
+*/
